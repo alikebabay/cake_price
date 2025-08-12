35f33783d5b8a4a3c0cf35229321db4fc7ac3907
alikebabay <aliketengri@gmail.com>
2025-08-12 19:54:21 +0500

    понизил болтливость бота

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
 logger = logging.getLogger(__name__)
-
+logging.getLogger("httpx").setLevel(logging.WARNING)
 
 #состояния - глубина меню
 MENU = 1
